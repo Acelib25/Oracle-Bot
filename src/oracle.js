714c6f242d98b37e13d931495d95200ca2fa0f4b
Iseden <isedenwildfire@gmail.com>
2022-08-19 12:49:06 -0500

    Sapphire

---
+++ src/oracle.js
@@ -0,0 +1,52 @@
+// Require the necessary discord.js classes
+const { GatewayIntentBits, Collection } = require('discord.js');
+const { token } = require('../.config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+const { SapphireClient } = require('@sapphire/framework');
+
+// Create a new client instance
+const client = new SapphireClient(
+    {
+        intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent],
+        defaultPrefix: 'Oracle,',
+    });
+
+// Create the Commands parts
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    console.log('Interaction Started');
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+	else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	}
+	else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
+
+// Login to Discord with your client's token
+client.login(token);
