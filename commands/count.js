4b268799ae05832877114254070de2156f178e8c
Iseden <isedenwildfire@gmail.com>
2022-08-31 21:43:41 -0500

    Oracle can count to 100

---
+++ commands/count.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('count')
+		.setDescription('Counts to number!')
+		.addIntegerOption(option => option.setName('target').setDescription('How high?')),
+	async execute(interaction) {
+		
+		const num = interaction.options.getInteger('target')
+
+		if (num > 100){
+			num = 100;
+		}
+
+		await interaction.reply(`Ok I will count to ${num}`)
+		await wait(3000);
+		for (let i = 1; i <= num; i++){
+			interaction.editReply(`${i}`)
+			await wait(1000);
+		}
+		interaction.editReply(`Done! ${num}`)
+		return { message: await interaction.fetchReply() }
+	},
+	async args(interaction) {
+		const num = interaction.options.getInteger('target')
+		return { target_number: num }
+	},
+};
